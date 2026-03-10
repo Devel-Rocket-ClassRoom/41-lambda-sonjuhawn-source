1fd020dafd135f29dd988602692bbb50b868e664
Son-JuHwan <sonjuhawn@gmail.com>
2026-03-10 14:41:30 +0900

    연습문제 2 3 완료

--- CodingPractice-02/Program.cs
+++ CodingPractice-02/Program.cs
@@ -1,4 +1,39 @@
 using System;
 
 // README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+Console.WriteLine("'''");
+{
+    Func<int> num = () => 1234;
+    Func<int, int> square = (a) => a * a;
+    Func<int ,int ,int> add = (a, b) => a + b;
+    Console.WriteLine(num());
+    Console.WriteLine(square(3));
+    Console.WriteLine(add(3, 5));
+}
+Console.WriteLine("'''\n");
+
+Console.WriteLine("'''");
+{
+    Action hi = () => Console.WriteLine("안녕하세요");
+    Action<string> hi1 = (str) => Console.WriteLine(str);
+    Action<string, int> hi2 = (str, num) =>
+    {
+        for (int i = 0; i < num; i++)
+        {
+            Console.WriteLine(str);
+        }
+    };
+    hi();
+    hi1("Hello, Lambda!");
+    hi2("반복", 3);
+}
+Console.WriteLine("'''\n");
+
+Console.WriteLine("'''");
+{
+    Func<int, int> anonymous = delegate (int x) { return x * x; };
+    Func<int, int> lambda = (x) => x * x;
+    anonymous(2);
+    lambda(2);
+}
+Console.WriteLine("'''\n");
--- CodingPractice-03/Program.cs
+++ CodingPractice-03/Program.cs
@@ -2,4 +2,89 @@ using System;
 using System.Collections.Generic;
 
 // README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+Console.WriteLine("'''");
+{
+    Predicate<int> num3 = (x) => x % 3 == 0;
+    var multi3 = FindNumbers(num3);
+    foreach(var x in multi3)
+    {
+        Console.Write($"{x} "); 
+    }
+    Console.WriteLine();
+    
+    var squares = FindNumbers(square =>
+        {
+        int sqrt = (int)Math.Sqrt(square);
+        return sqrt * sqrt == square;
+        });
+    foreach (var x in squares)
+    {
+        Console.Write($"{x} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("'''");
+
+Console.WriteLine("'''");
+{
+    Predicate<int> isEven = (x) => x % 2 == 0;
+    Func<int, bool> isOdd = (x) => (x % 2 == 1);
+
+    Console.WriteLine(isEven(4));
+    Console.WriteLine(isOdd(4));
+}
+Console.WriteLine("'''");
+
+Console.WriteLine("'''");
+{
+    int factor = 2;
+
+    Func<int, int> multiplier = n => n * factor;
+    Console.WriteLine(multiplier(3));
+    Console.WriteLine(multiplier(10));
+}
+Console.WriteLine("'''");
+
+Console.WriteLine("'''");
+{
+    int count = 0;
+
+    Action increment = () => count++;
+
+    increment();
+    increment();
+    increment();
+    Console.WriteLine(count);
+}
+Console.WriteLine("'''");
+
+Console.WriteLine("'''");
+{
+    var counter = CreateCounter();
+    Console.WriteLine(counter());
+    Console.WriteLine(counter());
+    Console.WriteLine(counter());
+
+}
+Console.WriteLine("'''");
+
+
+
+static List<int> FindNumbers(Predicate<int> predicate)
+{
+    List<int> ints = new List<int>();
+    for (int i = 1; i < 100; i++)
+    {
+        if (predicate(i))
+        {
+            ints.Add(i);
+        }
+    }
+    return ints;
+}
+
+static Func<int> CreateCounter()
+{
+    int count = 0;
+    return () => count++;
+}
